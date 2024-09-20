b6ebaaf865e68a8989393d0b3588bd2e6e215397
ebalo <ebalo55@users.noreply.github.com>
2024-09-20 17:44:06 +0200

    feat: certification page display

---
+++ src/content/config.ts
@@ -0,0 +1,26 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from "astro:content";
+
+// 2. Define your collection(s)
+const blogCollection = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      acronym: z.string(),
+      last_updated_at: z.date(),
+      image: image().optional(),
+      reference: z.string(),
+      aliases: z.array(z.string()).optional(),
+      draft: z.boolean().default(false),
+      career_paths: z.array(z.string()),
+      price: z.string(),
+      currency: z.string(),
+    }),
+});
+
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+  certifications: blogCollection,
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,17 @@
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
 import type { Config } from "tailwindcss";
 import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
-    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-    darkMode: "class",
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ["Nunito Variable", ...defaultTheme.fontFamily.sans],
-            },
-        },
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Nunito Variable", ...defaultTheme.fontFamily.sans],
+      },
     },
-    plugins: [],
+  },
+  plugins: [forms, typography],
 } as Config;
