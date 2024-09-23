90121f7c3370fdac946ba46c4d875f1f26773e73
ebalo <ebalo55@users.noreply.github.com>
2024-09-23 06:32:36 +0200

    fix: react svg syntax fix

--- src/components/icon/badge.tsx
+++ src/components/icon/badge.tsx
@@ -17,9 +17,9 @@ export const Badge: FC<ExtendedSvg> = ({
              viewBox="0 0 24 24"
              fill="none"
              stroke="currentColor"
-             stroke-width="2"
-             stroke-linecap="round"
-             stroke-linejoin="round"
+             strokeWidth="2"
+             strokeLinecap="round"
+             strokeLinejoin="round"
              className={ clsx([
                  "icon icon-tabler icons-tabler-outline icon-tabler-arrow-badge-right",
                  className,
