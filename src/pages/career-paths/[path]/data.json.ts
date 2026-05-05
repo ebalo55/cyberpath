20ff400ca0708156e90666b312e8c2abd3d0744a
ebalo55 <12586380+ebalo55@users.noreply.github.com>
2026-05-05 02:49:32 +0200

    refactor: improve code readability and consistency in API route handlers

--- src/pages/career-paths/[path]/data.json.ts
+++ src/pages/career-paths/[path]/data.json.ts
@@ -1,24 +1,18 @@
 import type { APIRoute } from "astro";
-import {
-    dash,
-} from "radash";
+import { dash } from "radash";
 import {
     CAREER_PATH_SPECIAL_HIDDEN,
     type CareerPathCollection,
-    CareerPathsList,
+    CareerPathsList
 } from "src/lib/career-paths.ts";
 
 /**
  * Get the paths for the static site generation.
  */
 export function getStaticPaths() {
-    const career_paths: CareerPathCollection = CareerPathsList.filter((entry) => {
-        return entry !== CAREER_PATH_SPECIAL_HIDDEN;
-    });
+    const career_paths: CareerPathCollection = CareerPathsList.filter((entry) => entry !== CAREER_PATH_SPECIAL_HIDDEN);
 
-    const kebab_career_paths = career_paths.map((career_path) => {
-        return dash(career_path);
-    });
+    const kebab_career_paths = career_paths.map((career_path) => dash(career_path));
 
     return kebab_career_paths.map((path) => ({
         params: {
@@ -30,6 +24,6 @@ export function getStaticPaths() {
 /**
  * Redirect to the certification database for career path metadata
  */
-export const GET: APIRoute = async ({ params, redirect }) => {
-    return redirect(`https://certdb.cyberpath-hq.com/career-paths/${params.path}/data.json`, 301);
-};
+export const GET: APIRoute = async({
+    params, redirect,
+}) => redirect(`https://certdb.cyberpath-hq.com/career-paths/${ params.path }/data.json`, 301);
